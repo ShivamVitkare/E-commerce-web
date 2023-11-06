17fe86c4515a88e9a3856febfc354feb8a84ed70
Shivam-Vitkare <120474507+ShivamVitkare@users.noreply.github.com>
2023-11-06 09:50:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./App.css";
+import Main from "./Components/Main/Main";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FilteredProducts from "./Components/FiltredProducts/FilteredProducts";
+import SingleProduct from "./Components/FiltredProducts/SingleProduct";
+
+import { useSelector } from "react-redux";
+import Cart from "./Components/Cart/Cart";
+
+function App() {
+  const user = useSelector((state) => state.user.user);
+  const { authUser } = user;
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={  <Main></Main> }
+          ></Route>
+         <Route  path="/Cart"
+            element={  <Cart></Cart> }>
+          
+         </Route>
+          <Route
+            path="/filteredProducts/:type"
+            element={<FilteredProducts></FilteredProducts>}
+          ></Route>
+          <Route
+            path="/filteredProducts/:type/:id"
+            element={<SingleProduct></SingleProduct>}
+          ></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
